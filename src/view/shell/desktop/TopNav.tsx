c867329a9b8da48bb0bd108ae9fd66697793708d
Scarlet <scarryaa@gmail.com>
2024-03-26 10:11:33 -0400

    added logos

--- src/view/shell/desktop/TopNav.tsx
+++ src/view/shell/desktop/TopNav.tsx
@@ -1,6 +1,8 @@
 import { useProfileQuery } from '../../../state/queries/profile';
 import { useSession } from '../../../state/session';
 import './TopNav.scss';
+import logo from '../../../../public/logo_icon.png';
+import { Link } from 'react-router-dom';
 
 const ProfileButton: React.FC = () => {
 	const { currentAccount } = useSession();
@@ -8,11 +10,19 @@ const ProfileButton: React.FC = () => {
 	return !isLoading && profile ? <img src={profile.avatar} className="profile-button" /> : <div>Placeholder</div>;
 };
 
+const LogoButton: React.FC = () => {
+	return (
+		<Link to={'/'}>
+			<img src={logo} className="top-nav__logo" />
+		</Link>
+	);
+};
+
 export const TopNav: React.FC = () => {
 	return (
 		<div className="top-nav">
 			<div className="top-nav__inner">
-				<div className="logo">logo</div>
+				<LogoButton />
 				<ProfileButton />
 			</div>
 		</div>
